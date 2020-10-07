d657e23ebc9849ebaaa38829e57c22ce34dadcd9
Bruno Sutic <code@brunosutic.com>
2020-10-07 12:27:20 +0200

    Add SlidingWindow specs

---
+++ spec/async/limiter/sliding_window/burstable_release_required_spec.rb
@@ -0,0 +1,258 @@
+require "async/limiter/sliding_window"
+
+RSpec.describe Async::Limiter::SlidingWindow do
+  describe "burstable, release required" do
+    let(:burstable) { true }
+    let(:release_required) { true }
+
+    include_examples :window_limiter
+
+    describe "#async" do
+      include_context :async_processing
+
+      context "when limit is 1" do
+        let(:limit) { 1 }
+        let(:repeats) { 3 }
+
+        context "when task duration is shorter than window" do
+          let(:task_duration) { 0.1 }
+
+          it "runs the tasks sequentially" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(100)],
+              ["task 1 start", be_within(50).of(1000)],
+              ["task 1 end", be_within(50).of(1100)],
+              ["task 2 start", be_within(50).of(2000)],
+              ["task 2 end", be_within(50).of(2100)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame == limit" do
+            expect(max_per_frame).to eq limit
+          end
+        end
+
+        context "when task duration is longer than window" do
+          let(:task_duration) { 1.5 }
+
+          it "runs the tasks sequentially" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(1500)],
+              ["task 1 start", be_within(50).of(1500)],
+              ["task 1 end", be_within(50).of(3000)],
+              ["task 2 start", be_within(50).of(3000)],
+              ["task 2 end", be_within(50).of(4500)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals the limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame equals 1" do
+            expect(max_per_frame).to eq 1
+          end
+        end
+      end
+
+      context "when limit is 3" do
+        let(:limit) { 3 }
+        let(:repeats) { 6 }
+        let(:task_duration) { 0.1 }
+
+        context "when task duration is shorter than window" do
+          let(:task_duration) { 0.1 }
+
+          it "runs the tasks concurrently until window is full" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(100)],
+              ["task 1 start", 0],
+              ["task 1 end", be_within(50).of(100)],
+              ["task 2 start", 0],
+              ["task 2 end", be_within(50).of(100)],
+              ["task 3 start", be_within(50).of(1000)],
+              ["task 3 end", be_within(50).of(1100)],
+              ["task 4 start", be_within(50).of(1000)],
+              ["task 4 end", be_within(50).of(1100)],
+              ["task 5 start", be_within(50).of(1000)],
+              ["task 5 end", be_within(50).of(1100)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals the limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame == limit" do
+            expect(max_per_frame).to eq limit
+          end
+        end
+
+        context "when task duration is longer than window" do
+          let(:task_duration) { 1.5 }
+
+          it "runs the tasks concurrently" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(1500)],
+              ["task 1 start", 0],
+              ["task 1 end", be_within(50).of(1500)],
+              ["task 2 start", 0],
+              ["task 2 end", be_within(50).of(1500)],
+              ["task 3 start", be_within(50).of(1500)],
+              ["task 3 end", be_within(50).of(3000)],
+              ["task 4 start", be_within(50).of(1500)],
+              ["task 4 end", be_within(50).of(3000)],
+              ["task 5 start", be_within(50).of(1500)],
+              ["task 5 end", be_within(50).of(3000)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks is equals the limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame == limit" do
+            expect(max_per_frame).to eq limit
+          end
+        end
+      end
+    end
+
+    describe "#blocking?" do
+      include_context :blocking_contexts
+
+      before do
+        wait_until_next_fixed_window_start
+      end
+
+      context "with a default limiter" do
+        context "when no locks are acquired" do
+          include_examples :limiter_is_not_blocking
+        end
+
+        context "when a single lock is acquired" do
+          include_context :single_lock_is_acquired
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_blocking
+
+            context "after release" do
+              before { limiter.release }
+              include_examples :limiter_is_not_blocking
+            end
+          end
+        end
+
+        context "when all the locks are released immediately" do
+          include_context :all_locks_are_released_immediately
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+
+        context "when no locks are released until the next window" do
+          include_context :no_locks_are_released_until_next_window
+          include_examples :limiter_is_blocking
+
+          context "after release" do
+            before { limiter.release }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+      end
+
+      context "when limit is 2" do
+        let(:limit) { 2 }
+
+        context "when no locks are acquired" do
+          include_examples :limiter_is_not_blocking
+        end
+
+        context "when a single lock is acquired" do
+          include_context :single_lock_is_acquired
+          include_examples :limiter_is_not_blocking
+        end
+
+        context "when all the locks are acquired" do
+          include_context :all_locks_are_acquired
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_blocking
+
+            context "after release" do
+              before { limiter.release }
+              include_examples :limiter_is_not_blocking
+            end
+          end
+        end
+
+        context "when all the locks are released immediately" do
+          include_context :all_locks_are_released_immediately
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+
+        context "when no locks are released until the next window" do
+          include_context :no_locks_are_released_until_next_window
+          include_examples :limiter_is_blocking
+
+          context "after release" do
+            before { limiter.release }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/async/limiter/sliding_window/non_burstable_release_required_spec.rb
@@ -0,0 +1,249 @@
+require "async/limiter/sliding_window"
+
+RSpec.describe Async::Limiter::SlidingWindow do
+  describe "non burstable, release required" do
+    let(:burstable) { false }
+    let(:release_required) { true }
+
+    include_examples :window_limiter
+
+    describe "#async" do
+      include_context :async_processing
+
+      context "when limit is 1" do
+        let(:limit) { 1 }
+        let(:repeats) { 3 }
+
+        context "when task duration is shorter than window" do
+          let(:task_duration) { 0.1 }
+
+          it "executes the tasks sequentially" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(100)],
+              ["task 1 start", be_within(50).of(1000)],
+              ["task 1 end", be_within(50).of(1100)],
+              ["task 2 start", be_within(50).of(2000)],
+              ["task 2 end", be_within(50).of(2100)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame equals 1" do
+            expect(max_per_frame).to eq 1
+          end
+        end
+
+        context "when task duration is longer than window" do
+          let(:task_duration) { 1.5 }
+
+          it "executes the tasks sequentially" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(1500)],
+              ["task 1 start", be_within(50).of(1500)],
+              ["task 1 end", be_within(50).of(3000)],
+              ["task 2 start", be_within(50).of(3000)],
+              ["task 2 end", be_within(50).of(4500)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals the limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame equals 1" do
+            expect(max_per_frame).to eq 1
+          end
+        end
+      end
+
+      context "when limit is 3" do
+        let(:limit) { 3 } # window_frame is 1.0 / 3 = 0.33
+        let(:repeats) { 6 }
+
+        context "when task duration is shorter than window frame" do
+          let(:task_duration) { 0.1 }
+
+          it "executes the tasks sequentially" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 0 end", be_within(50).of(100)],
+              ["task 1 start", be_within(50).of(333)],
+              ["task 1 end", be_within(50).of(433)],
+              ["task 2 start", be_within(50).of(666)],
+              ["task 2 end", be_within(50).of(766)],
+              ["task 3 start", be_within(50).of(1000)],
+              ["task 3 end", be_within(50).of(1100)],
+              ["task 4 start", be_within(50).of(1333)],
+              ["task 4 end", be_within(50).of(1433)],
+              ["task 5 start", be_within(50).of(1666)],
+              ["task 5 end", be_within(50).of(1766)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks is 1" do
+            expect(maximum).to eq 1
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame equals 1" do
+            expect(max_per_frame).to eq 1
+          end
+        end
+
+        context "when task duration is longer than window frame" do
+          let(:task_duration) { 1.5 }
+
+          # spec with intermittent failures
+          it "intermingles task execution" do
+            expect(task_stats).to contain_exactly(
+              ["task 0 start", 0],
+              ["task 1 start", be_within(50).of(333)],
+              ["task 2 start", be_within(50).of(666)],
+              ["task 0 end", be_within(50).of(1500)], # resumes task 3
+              ["task 3 start", be_within(50).of(1500)],
+              ["task 1 end", be_within(50).of(1833)], # resumes task 4
+              ["task 4 start", be_within(50).of(1833)],
+              ["task 2 end", be_within(50).of(2166)], # resumes task 5
+              ["task 5 start", be_within(50).of(2166)],
+              ["task 3 end", be_within(50).of(3000)],
+              ["task 4 end", be_within(50).of(3333)],
+              ["task 5 end", be_within(50).of(3666)]
+            )
+          end
+
+          it "ensures max number of concurrent tasks equals the limit" do
+            expect(maximum).to eq limit
+          end
+
+          it "ensures the results are in the correct order" do
+            expect(result).to eq (0...repeats).to_a
+          end
+
+          it "ensures max number of started tasks in a window == limit" do
+            expect(max_per_second).to eq limit
+          end
+
+          it "ensures max number of started tasks in a window frame equals 1" do
+            expect(max_per_frame).to eq 1
+          end
+        end
+      end
+    end
+
+    describe "#blocking?" do
+      include_context :blocking_contexts
+
+      before do
+        wait_until_next_fixed_window_start
+      end
+
+      context "with a default limit" do
+        context "when no locks are acquired" do
+          include_examples :limiter_is_not_blocking
+        end
+
+        context "when a single lock is acquired" do
+          include_context :single_lock_is_acquired
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_blocking
+          end
+        end
+
+        context "when all the locks are released immediately" do
+          include_context :all_locks_are_released_immediately
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+
+        context "when no locks are released until the next window" do
+          include_context :no_locks_are_released_until_next_window
+          include_examples :limiter_is_blocking
+
+          after do
+            limiter.release
+            expect(limiter).not_to be_blocking
+          end
+        end
+      end
+
+      context "when limit is 2" do
+        let(:limit) { 2 }
+        let(:window_frame) { window.to_f / limit }
+
+        context "when no locks are acquired" do
+          include_examples :limiter_is_not_blocking
+        end
+
+        context "when a single lock is acquired" do
+          include_context :single_lock_is_acquired
+          include_examples :limiter_is_blocking
+
+          context "after window frame passes" do
+            before { wait_until_next_window_frame }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+
+        context "when all the locks are acquired" do
+          include_context :all_locks_are_acquired
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_blocking
+          end
+        end
+
+        context "when all the locks are released immediately" do
+          include_context :all_locks_are_released_immediately
+          include_examples :limiter_is_blocking
+
+          context "after window passes" do
+            before { wait_until_next_window }
+            include_examples :limiter_is_not_blocking
+          end
+        end
+
+        context "when no locks are released until the next window" do
+          include_context :no_locks_are_released_until_next_window
+          include_examples :limiter_is_blocking
+        end
+      end
+    end
+  end
+end
