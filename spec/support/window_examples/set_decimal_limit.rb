3a3438db0fcb27d9447f76964614ad770ee33769
Bruno Sutic <code@brunosutic.com>
2020-10-07 17:53:59 +0200

    Improve 'set decimal limit' specs

--- spec/support/window_examples/set_decimal_limit.rb
+++ spec/support/window_examples/set_decimal_limit.rb
@@ -1,3 +1,80 @@
+RSpec.shared_examples :set_decimal_limit_smaller_than_1 do
+  context "smaller than 1" do
+    let(:new_window) { window.to_f * 1 / new_limit }
+    let(:new_limit) { 0.5 }
+
+    context "when starting limit is 1" do
+      let(:limit) { 1 }
+
+      it "sets limit to 1 and window to 2" do
+        expect(new_window).to eq 2
+
+        expect(task_stats).to contain_exactly(
+          ["task 0 start", 0],
+          ["task 0 end", be_within(50).of(100)],
+          ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
+          ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
+          ["task 2 start", be_within(50).of(new_window * 1000 * 2)], # 4000
+          ["task 2 end", be_within(50).of(new_window * 1000 * 2 + 100)]
+        )
+      end
+    end
+
+    context "when starting limit is 3" do
+      let(:limit) { 3 }
+
+      it "sets limit to 1 and window to 2" do
+        expect(new_window).to eq 2
+
+        expect(task_stats).to contain_exactly(
+          ["task 0 start", 0],
+          ["task 0 end", be_within(50).of(100)],
+          ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
+          ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
+          ["task 2 start", be_within(50).of(new_window * 1000 * 2)], # 4000
+          ["task 2 end", be_within(50).of(new_window * 1000 * 2 + 100)]
+        )
+      end
+    end
+  end
+end
+
+RSpec.shared_examples :set_decimal_limit_greater_than_1_window_3 do
+  context "when window is 3" do
+    let(:window) { 3 }
+    let(:new_window) { window.to_f * new_limit.floor / new_limit }
+
+    shared_examples :sets_limit_1_window_2 do
+      it "sets limit to 1 and window to 2" do
+        expect(new_window).to eq 2
+
+        expect(task_stats).to contain_exactly(
+          ["task 0 start", 0],
+          ["task 0 end", be_within(50).of(100)],
+          ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
+          ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
+          ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
+          ["task 2 end", be_within(50).of(new_window * 2000 + 100)],
+          ["task 3 start", be_within(50).of(new_window * 3000)], # 6000
+          ["task 3 end", be_within(50).of(new_window * 3000 + 100)]
+        )
+      end
+    end
+
+    context "when starting limit is 1" do
+      let(:limit) { 1 }
+
+      include_examples :sets_limit_1_window_2
+    end
+
+    context "when starting limit is 3" do
+      let(:limit) { 3 }
+
+      include_examples :sets_limit_1_window_2
+    end
+  end
+end
+
 RSpec.shared_examples :set_decimal_limit_burstable do
   describe "#limit=" do
     context "when new limit is a decimal number" do
@@ -9,55 +86,18 @@ RSpec.shared_examples :set_decimal_limit_burstable do
 
       include_context :async_processing
 
-      context "smaller than 1" do
-        let(:new_window) { window.to_f * 1 / new_limit }
-        let(:new_limit) { 0.5 }
-
-        context "when starting limit is 1" do
-          let(:limit) { 1 }
-
-          it "sets limit to 1 and window to 2" do
-            expect(new_window).to eq 2
-
-            expect(task_stats).to contain_exactly(
-              ["task 0 start", 0],
-              ["task 0 end", be_within(50).of(100)],
-              ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-              ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-              ["task 2 start", be_within(50).of(new_window * 1000 * 2)], # 4000
-              ["task 2 end", be_within(50).of(new_window * 1000 * 2 + 100)]
-            )
-          end
-        end
-
-        context "when starting limit is 3" do
-          let(:limit) { 3 }
-
-          it "sets limit to 1 and window to 2" do
-            expect(new_window).to eq 2
-
-            expect(task_stats).to contain_exactly(
-              ["task 0 start", 0],
-              ["task 0 end", be_within(50).of(100)],
-              ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-              ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-              ["task 2 start", be_within(50).of(new_window * 1000 * 2)], # 4000
-              ["task 2 end", be_within(50).of(new_window * 1000 * 2 + 100)]
-            )
-          end
-        end
-      end
+      include_examples :set_decimal_limit_smaller_than_1
 
       context "greater than 1" do
         let(:new_limit) { 1.5 }
         let(:repeats) { 4 }
 
+        include_examples :set_decimal_limit_greater_than_1_window_3
+
         context "when window is 1" do
           let(:new_window) { window.to_f * new_limit.ceil / new_limit }
 
-          context "when starting limit is 1" do
-            let(:limit) { 1 }
-
+          shared_examples :sets_limit_2_window_1_33 do
             it "sets limit to 2 and window to 1.33" do
               expect(new_window.truncate(2)).to eq 1.33
 
@@ -74,66 +114,16 @@ RSpec.shared_examples :set_decimal_limit_burstable do
             end
           end
 
-          context "when starting limit is 3" do
-            let(:limit) { 3 }
-
-            it "sets limit to 2 and window to 1.33" do
-              expect(new_window.truncate(2)).to eq 1.33
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", 0],
-                ["task 1 end", be_within(50).of(100)],
-                ["task 2 start", be_within(50).of(new_window * 1000)], # 1333
-                ["task 2 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 1000)], # 1333
-                ["task 3 end", be_within(50).of(new_window * 1000 + 100)]
-              )
-            end
-          end
-        end
-
-        context "when window is 3" do
-          let(:window) { 3 }
-          let(:new_window) { window.to_f * new_limit.floor / new_limit }
-
           context "when starting limit is 1" do
             let(:limit) { 1 }
 
-            it "sets limit to 1 and window to 2" do
-              expect(new_window).to eq 2
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-                ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-                ["task 2 end", be_within(50).of(new_window * 2000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 3000)], # 6000
-                ["task 3 end", be_within(50).of(new_window * 3000 + 100)]
-              )
-            end
+            include_examples :sets_limit_2_window_1_33
           end
 
           context "when starting limit is 3" do
             let(:limit) { 3 }
 
-            it "sets limit to 1 and window to 2" do
-              expect(new_window).to eq 2
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-                ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-                ["task 2 end", be_within(50).of(new_window * 2000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 3000)], # 6000
-                ["task 3 end", be_within(50).of(new_window * 3000 + 100)]
-              )
-            end
+            include_examples :sets_limit_2_window_1_33
           end
         end
       end
@@ -152,55 +142,18 @@ RSpec.shared_examples :set_decimal_limit_non_burstable do
 
       include_context :async_processing
 
-      context "smaller than 1" do
-        let(:new_window) { window.to_f * 1 / new_limit }
-        let(:new_limit) { 0.5 }
-
-        context "when starting limit is 1" do
-          let(:limit) { 1 }
-
-          it "sets limit to 1 and window to 2" do
-            expect(new_window).to eq 2
-
-            expect(task_stats).to contain_exactly(
-              ["task 0 start", 0],
-              ["task 0 end", be_within(50).of(100)],
-              ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-              ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-              ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-              ["task 2 end", be_within(50).of(new_window * 2000 + 100)]
-            )
-          end
-        end
-
-        context "when starting limit is 3" do
-          let(:limit) { 3 }
-
-          it "sets limit to 1 and window to 2" do
-            expect(new_window).to eq 2
-
-            expect(task_stats).to contain_exactly(
-              ["task 0 start", 0],
-              ["task 0 end", be_within(50).of(100)],
-              ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-              ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-              ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-              ["task 2 end", be_within(50).of(new_window * 2000 + 100)]
-            )
-          end
-        end
-      end
+      include_examples :set_decimal_limit_smaller_than_1
 
       context "greater than 1" do
         let(:new_limit) { 1.5 }
         let(:repeats) { 4 }
 
+        include_examples :set_decimal_limit_greater_than_1_window_3
+
         context "when window is 1" do
           let(:new_window) { window.to_f * new_limit.ceil / new_limit }
 
-          context "when starting limit is 1" do
-            let(:limit) { 1 }
-
+          shared_examples :sets_limit_2_window_1_33 do
             it "sets limit to 2 and window to 1.33" do
               expect(new_window.truncate(2)).to eq 1.33
 
@@ -217,66 +170,16 @@ RSpec.shared_examples :set_decimal_limit_non_burstable do
             end
           end
 
-          context "when starting limit is 3" do
-            let(:limit) { 3 }
-
-            it "sets limit to 2 and window to 1.33" do
-              expect(new_window.truncate(2)).to eq 1.33
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", be_within(50).of(666)],
-                ["task 1 end", be_within(50).of(766)],
-                ["task 2 start", be_within(50).of(new_window * 1000)], # 1333
-                ["task 2 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 1000 + 666)],
-                ["task 3 end", be_within(50).of(new_window * 1000 + 766)]
-              )
-            end
-          end
-        end
-
-        context "when window is 3" do
-          let(:window) { 3 }
-          let(:new_window) { window.to_f * new_limit.floor / new_limit }
-
           context "when starting limit is 1" do
             let(:limit) { 1 }
 
-            it "sets limit to 1 and window to 2" do
-              expect(new_window).to eq 2
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-                ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-                ["task 2 end", be_within(50).of(new_window * 2000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 3000)], # 6000
-                ["task 3 end", be_within(50).of(new_window * 3000 + 100)]
-              )
-            end
+            include_examples :sets_limit_2_window_1_33
           end
 
           context "when starting limit is 3" do
             let(:limit) { 3 }
 
-            it "sets limit to 1 and window to 2" do
-              expect(new_window).to eq 2
-
-              expect(task_stats).to contain_exactly(
-                ["task 0 start", 0],
-                ["task 0 end", be_within(50).of(100)],
-                ["task 1 start", be_within(50).of(new_window * 1000)], # 2000
-                ["task 1 end", be_within(50).of(new_window * 1000 + 100)],
-                ["task 2 start", be_within(50).of(new_window * 2000)], # 4000
-                ["task 2 end", be_within(50).of(new_window * 2000 + 100)],
-                ["task 3 start", be_within(50).of(new_window * 3000)], # 6000
-                ["task 3 end", be_within(50).of(new_window * 3000 + 100)]
-              )
-            end
+            include_examples :sets_limit_2_window_1_33
           end
         end
       end
