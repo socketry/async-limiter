4677ca98444fb1d143da709c39921feffe081ced
Bruno Sutic <code@brunosutic.com>
2020-10-08 03:28:34 +0200

    Fix SlidingWindow#initialize

--- lib/async/limiter/sliding_window.rb
+++ lib/async/limiter/sliding_window.rb
@@ -6,7 +6,7 @@ module Async
     # Example: You can perform N operations at 10:10:10.999 but can't perform
     # another N operations until 10:10:11.999.
     class SlidingWindow < Window
-      def initialize(...)
+      def initialize(*args, **options)
         raise "Don't specify :type with #{self.class}" if options[:type]
 
         super(*args, type: :sliding, **options)
